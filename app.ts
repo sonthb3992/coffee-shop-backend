dd2dde22f710b07198ff7dbcaba5c65ff2302501
sonthb3992 <sonthb@avnaudit.vn>
2023-05-28 09:18:57 +0700

    Create the backend app

---
+++ app.ts
@@ -0,0 +1,12 @@
+import express = require("express");
+
+const app = express();
+
+app.get('/api/hello', (req, res) => {
+    res.json({ message: 'Hello, World!' });
+});
+
+const port = 3005;
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
