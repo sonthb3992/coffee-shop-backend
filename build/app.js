540348c3ffa4708c4bc442d6f409a204d7291e21
sonthb3992 <71615947+sonthb3992@users.noreply.github.com>
2023-05-28 10:22:39 +0700

    Update app.js

--- build/app.js
+++ build/app.js
@@ -5,7 +5,7 @@ const app = express();
 app.get('/api/hello', (req, res) => {
     res.json({ message: 'Hello, World!' });
 });
-const port = process.env.PORT || 3000;
+const port = 7000;
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
